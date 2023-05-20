ecf5a214bce1e9d327f8d82f53abb006e5825b1e
Eduardo de Lima <eduardodelimaex@gmail.com>
2023-05-20 02:44:16 -0300

    frontend rocketnotes

---
+++ rocketnotes/src/pages/Details/index.jsx
@@ -0,0 +1,11 @@
+import {Container} from "./styles"
+
+export function Details(){
+
+
+  return(
+    <Container>
+    <h1>Seven two three two three.. send</h1>
+    </Container>
+  )
+}
---
+++ rocketnotes/src/pages/Details/styles.js
@@ -0,0 +1,12 @@
+import styled from "styled-components"
+
+
+
+ export const Container = styled.div`
+
+background: rgb(202, 165, 165);
+`
+
+
+
+
