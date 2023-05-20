687982cde1de6655e9dbdc68d8fd737c2929e420
Eduardo de Lima <eduardodelimaex@gmail.com>
2023-05-20 13:02:25 -0300

    buttons

--- rocketnotes/src/pages/Details/index.jsx
+++ rocketnotes/src/pages/Details/index.jsx
@@ -1,11 +1,16 @@
 import {Container} from "./styles"
 
+import { Button } from "../../components/button"
+
 export function Details(){
 
 
   return(
     <Container>
     <h1>Seven two three two three.. send</h1>
+    <Button title = "Entrar"  />
+    <Button title = "Cadastrar"/>
+    <Button title = "Voltar"/>
     </Container>
   )
 }
