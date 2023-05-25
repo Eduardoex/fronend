5eda86e3643cd29ee66765c8cdc8c84bd3a53705
Eduardo de Lima <eduardodelimaex@gmail.com>
2023-05-25 00:37:20 -0300

    tags / section

--- rocketnotes/src/pages/Details/index.jsx
+++ rocketnotes/src/pages/Details/index.jsx
@@ -1,7 +1,9 @@
-import {Container} from "./styles"
+import {Container, Links} from "./styles"
 
 import { Header } from "../../components/header"
 import { Button } from "../../components/button"
+import { Section } from "../../components/section"
+import { Tag } from "../../components/tag"
 
 export function Details(){
 
@@ -9,6 +11,19 @@ export function Details(){
   return(
     <Container>
       <Header />
+      <Section title="Links úteis">
+        <Links>
+          <li><a href="#">https://www.rocketseat.com.br</a></li>
+          <li><a href="#">https://www.rocketseat.com.br</a></li>
+        </Links>
+      </Section>
+
+
+      <Section title="Marcadores">
+        <Tag title="express"></Tag>
+        <Tag title="express"></Tag>
+
+      </Section>
    
    
     <Button title = "Voltar"/>
