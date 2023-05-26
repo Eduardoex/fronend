7300748f412c6ccf792ff6624f2280626e970e6d
Eduardo de Lima <eduardodelimaex@gmail.com>
2023-05-26 02:33:34 -0300

    buttontext

--- rocketnotes/src/pages/Details/styles.js
+++ rocketnotes/src/pages/Details/styles.js
@@ -10,7 +10,13 @@ import styled from "styled-components"
  grid-template-rows: 105px auto;
  grid-template-areas: 
  "Header"
- "content"
+ "content";
+
+ > main {
+  grid-area: content;
+  overflow-y: scroll;
+  padding: 64px 0;
+ }
  
  `
 export const Links = styled.ul`
@@ -23,3 +29,28 @@ list-style: none;
     color: ${({theme}) => theme.COLORS.WHITE}
   }
 }`
+
+export const Content = styled.div`
+  max-width: 550px;
+  margin: 0 auto;
+
+  display: flex;
+  flex-direction: column;
+
+  > button:first-child{
+    align-self: end;
+    
+  }
+  
+  > h1 {
+    font-size: 36px;
+    font-weight: 500;
+    padding-top: 64px;
+  }
+
+  > p {
+    font-size: 16px;
+    margin-top: 16px;
+    text-align: justify;
+  }
+`
